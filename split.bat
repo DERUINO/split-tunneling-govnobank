d819c26a34c1a7d737b80d918cf0fd64178426cf
DERUINO <flashnickel@gmail.com>
2024-11-14 19:10:33 +0300

    fix path for openning programs

--- split.bat
+++ split.bat
@@ -13,14 +13,14 @@ for /F "tokens=2 delims=," %%P in ('
     tasklist /SVC /FI "imagename eq Telegram.exe" /FO CSV /NH
 ') do (
     taskkill /PID %%P% /F
-    "ForceBindIP64.exe" %interface_ip% "C:\Users\flash\AppData\Roaming\Telegram Desktop\Telegram.exe"
+    "ForceBindIP64.exe" %interface_ip% "C:\Users\%USERNAME%\AppData\Roaming\Telegram Desktop\Telegram.exe"
 )
 
 for /F "tokens=2 delims=," %%P in ('
     tasklist /SVC /FI "imagename eq vkteams.exe" /FO CSV /NH
 ') do (
     taskkill /PID %%P% /F
-    "ForceBindIP64.exe" %interface_ip% "C:\Users\flash\AppData\Local\Programs\VK Teams\vkteams.exe"
+    "ForceBindIP64.exe" %interface_ip% "C:\Users\%USERNAME%\AppData\Local\Programs\VK Teams\vkteams.exe"
 )
 
 for /F "tokens=2 delims=," %%P in ('
